f8d9c6724b6b902b504ed83c0176a035ae18f85d
deeptishukla86 <89309008+deeptishukla86@users.noreply.github.com>
2021-12-09 02:20:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Inheritance
+{
+   public  class SBI
+
+      
+    {
+        public float rate_sbi = 6.5f;
+        public double calc_Sbi(double amount ,int years)
+        {
+            return amount = ((amount * rate_sbi * years) / 100 + (amount));
+
+        }
+    }
+   public class HDFC:SBI
+    {
+        public float rate_HDFC = 8.5f;
+        public double calc_HDFC(double amount, int years)
+        {
+            return amount = ((amount * rate_HDFC * years) / 100+(amount));
+        }
+    }
+    public class PNB : SBI
+    {
+        public float rate_PNB = 5.0f;
+        public double calc_PNB(double amount, int years)
+        {
+            return amount = ((amount * rate_PNB * years) / 100 +(amount));
+        }
+    }
+
+
+    class abc { 
+        static void Main(string[] args)
+        {
+            PNB f = new PNB();
+            HDFC h = new HDFC();
+            Console.WriteLine("enter yr amount");
+            double amount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("for how many years ");
+            int years = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("your amount accoring to SBI after" + years + "year will be " + f.calc_Sbi(amount, years));
+            Console.WriteLine("your amount accoring to HDFC after" + years + "year will be " + h.calc_HDFC(amount, years));
+            Console.WriteLine("your amount accoring to PNB after" + years + "year will be " + f.calc_PNB(amount, years));
+
+        }
+    }
+}
